3af0e61a0d7f0c5b8622a03b3561dc3373379794
rajkumar <rajkumarrajkumar061@gmail.com>
2024-02-16 00:41:17 +0530

    changes

---
+++ forms/feedback.php
@@ -0,0 +1,34 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "madurailpa";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // Insert data into the database
+    $sql = "INSERT INTO feedback (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
+
+// Close connection
+$conn->close();
+?>
--- forms/survey.php
+++ forms/survey.php
@@ -11,6 +11,7 @@ if ($conn->connect_error) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  var_dump($_POST);
   $age = $_POST['age'];
   $sex = $_POST['sex'];
   $locality = $_POST['locality'];
@@ -25,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $PreferredPublicTransport = $_POST['PreferredPublicTransport'];
   $ParkingFacilityHome = $_POST['ParkingFacilityHome'];
   $ParkingFacilitiesPublic = $_POST['ParkingFacilitiesPublic'];
-  $ElectricVehicleOption = isset($_POST['ElectricVehicleOption']) ? $_POST['ElectricVehicleOption'] : "";
+  $ElectricVehicleOption = $_POST['ElectricVehicleOption'];
   $ElectricVehicleReason = isset($_POST['ElectricVehicleReason']) ? $_POST['ElectricVehicleReason'] : "";
   $ChargingFacilities = $_POST['ChargingFacilities'];
   $DedicatedCycleTrack = $_POST['DedicatedCycleTrack'];
@@ -37,36 +38,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $AlternativeTransportInitiatives = $_POST['AlternativeTransportInitiatives'];
   $PublicTransportSatisfaction = $_POST['PublicTransportSatisfaction'];
   $WaterSupply = $_POST['WaterSupply'];
-  $WaterSupplyFrequency = $_POST['WaterSupplyFrequency'];
+  $WaterSupplyFrequency = isset($_POST['WaterSupplyFrequency']) ? $_POST['WaterSupplyFrequency'] : "";
   $WaterStagnation = $_POST['WaterStagnation'];
   $SewageTreatment = $_POST['SewageTreatment'];
   $WasteSegregation = $_POST['waste_segregation'];
   $DecentralizationSolidWaste = $_POST['decentralization_solid_waste'];
   $SolidWasteSuggestions = $_POST['solid_waste_suggestions'];
-  $NeighborhoodSafetyNight = $_POST['neighborhood_safety_night'];
-  $EmergencyServicesSatisfaction = $_POST['emergency_services_satisfaction'];
-  $PoliceServicesSatisfaction = $_POST['police_services_satisfaction'];
-  $FireServicesSatisfaction = $_POST['fire_services_satisfaction'];
-  $MedicalServicesSatisfaction = $_POST['medical_services_satisfaction'];
-  $EducationalCulturalOpportunities = $_POST['educational_cultural_opportunities'];
-  $ParksSatisfaction = $_POST['parks_satisfaction'];
-  $DistanceToPark = $_POST['distance_to_park'];
-  $RecreationalFacilities = $_POST['recreational_facilities'];
-  $ClimateChangeConcern = $_POST['climate_change_concern'];
-  $UtilizationAbandonedQuarry = $_POST['utilization_abandoned_quarry'];
-  $RevampingWaterBodies = $_POST['revamping_water_bodies'];
-  $DesiltingDrainChannels = $_POST['desilting_drain_channels'];
-  $RevitalizationWetlands = $_POST['revitalization_wetlands'];
-  $EcoPark = $_POST['eco_park'];
-  $Others = $_POST['others'];
-  $TourismCircuit = $_POST['tourism_circuit'];
-  $LocalBusinessInitiatives = $_POST['local_business_initiatives'];
-  $AdditionalSuggestions = $_POST['additional_suggestions'];
+  $neighborhoodSafetyNight = $_POST["neighborhood_safety_night"];
+  $emergencyServicesSatisfaction = $_POST["emergency_services_satisfaction"];
+  $policeServicesSatisfaction = $_POST["police_services_satisfaction"];
+  $fireServicesSatisfaction = $_POST["fire_services_satisfaction"];
+  $medicalServicesSatisfaction = $_POST["medical_services_satisfaction"];
+  $educationalCulturalOpportunities = $_POST["educational_cultural_opportunities"];
+  $parksSatisfaction = $_POST["parks_satisfaction"];
+  $distanceToPark = $_POST["distance_to_park"];
+  $recreationalFacilities = $_POST["recreational_facilities"];
+  $climateChangeConcern = $_POST["climate_change_concern"];
+  $utilizationAbandonedQuarry = $_POST["utilization_abandoned_quarry"];
+  $revampingWaterBodies = $_POST["revamping_water_bodies"];
+  $desiltingDrainChannels = $_POST["desilting_drain_channels"];
+  $revitalizationWetlands = $_POST["revitalization_wetlands"];
+  $ecoPark = $_POST["eco_park"];
+  $others = $_POST["others"];
+  $tourismCircuit = $_POST["tourism_circuit"];
+  $localBusinessInitiatives = $_POST["local_business_initiatives"];
+  $additionalSuggestions = $_POST["additional_suggestions"];
 
-  // Prepare the SQL query
-  $sql = "INSERT INTO survey 
-          (age, sex, locality, occupation, income, KnowaboutMasterPlan, ColdStorage, LogisticHub, EconomySatisfaction, DailyTransport, TravelDistance, PreferredPublicTransport, ParkingFacilityHome, ParkingFacilitiesPublic, ElectricVehicleOption, ElectricVehicleReason, ChargingFacilities, DedicatedCycleTrack, NoVehiclesZone, PedestrianWalkways, TrafficJunctions, ParaTransitJunctions, NewSubArterialRoad, AlternativeTransportInitiatives, PublicTransportSatisfaction, WaterSupply, WaterSupplyFrequency, WaterStagnation, SewageTreatment, WasteSegregation, DecentralizationSolidWaste, SolidWasteSuggestions, NeighborhoodSafetyNight, EmergencyServicesSatisfaction, PoliceServicesSatisfaction, FireServicesSatisfaction, MedicalServicesSatisfaction, EducationalCulturalOpportunities, ParksSatisfaction, DistanceToPark, RecreationalFacilities, ClimateChangeConcern, UtilizationAbandonedQuarry, RevampingWaterBodies, DesiltingDrainChannels, RevitalizationWetlands, EcoPark, Others, TourismCircuit, LocalBusinessInitiatives, AdditionalSuggestions) 
-          VALUES ( '$age', '$sex', '$locality', '$occupation', '$income', '$KnowaboutMasterPlan', '$ColdStorage', '$LogisticHub', '$EconomySatisfaction', '$DailyTransport', '$TravelDistance', '$PreferredPublicTransport', '$ParkingFacilityHome', '$ParkingFacilitiesPublic', '$ElectricVehicleOption', '$ElectricVehicleReason', '$ChargingFacilities', '$DedicatedCycleTrack', '$NoVehiclesZone', '$PedestrianWalkways', '$TrafficJunctions', '$ParaTransitJunctions', '$NewSubArterialRoad', '$AlternativeTransportInitiatives', '$PublicTransportSatisfaction', '$WaterSupply', '$WaterSupplyFrequency', '$WaterStagnation', '$SewageTreatment', '$WasteSegregation', '$DecentralizationSolidWaste', '$SolidWasteSuggestions', '$NeighborhoodSafetyNight', '$EmergencyServicesSatisfaction', '$PoliceServicesSatisfaction', '$FireServicesSatisfaction', '$MedicalServicesSatisfaction', '$EducationalCulturalOpportunities', '$ParksSatisfaction', '$DistanceToPark', '$RecreationalFacilities', '$ClimateChangeConcern', '$UtilizationAbandonedQuarry', '$RevampingWaterBodies', '$DesiltingDrainChannels', '$RevitalizationWetlands', '$EcoPark', '$Others', '$TourismCircuit', '$LocalBusinessInitiatives', '$AdditionalSuggestions')";
+  $sql = "INSERT INTO survey (age, sex, locality, occupation, income,KnowaboutMasterPlan, ColdStorage, LogisticHub, EconomySatisfaction, DailyTransport, TravelDistance, PreferredPublicTransport, ParkingFacilityHome,ParkingFacilitiesPublic, ElectricVehicleOption, ElectricVehicleReason, ChargingFacilities, DedicatedCycleTrack, NoVehiclesZone, PedestrianWalkways, TrafficJunctions, ParaTransitJunctions, NewSubArterialRoad,AlternativeTransportInitiatives, PublicTransportSatisfaction, WaterSupply, WaterSupplyFrequency, WaterStagnation, SewageTreatment, WasteSegregation, DecentralizationSolidWaste, SolidWasteSuggestions,neighborhoodSafetyNight, emergencyServicesSatisfaction, policeServicesSatisfaction, fireServicesSatisfaction, medicalServicesSatisfaction, educationalCulturalOpportunities,parksSatisfaction, distanceToPark, recreationalFacilities, climateChangeConcern, utilizationAbandonedQuarry, revampingWaterBodies,DesiltingDrainChannels, RevitalizationWetlands, EcoPark, Others, TourismCircuit, 
+  LocalBusinessInitiatives, AdditionalSuggestions) VALUES ('$age', '$sex', '$locality', '$occupation', '$income','$KnowaboutMasterPlan', '$ColdStorage', '$LogisticHub', '$EconomySatisfaction', '$DailyTransport', '$TravelDistance', '$PreferredPublicTransport', '$ParkingFacilityHome','$ParkingFacilitiesPublic', '$ElectricVehicleOption', '$ElectricVehicleReason', '$ChargingFacilities', '$DedicatedCycleTrack', '$NoVehiclesZone', '$PedestrianWalkways', '$TrafficJunctions', '$ParaTransitJunctions', '$NewSubArterialRoad','$AlternativeTransportInitiatives', '$PublicTransportSatisfaction', '$WaterSupply', '$WaterSupplyFrequency', '$WaterStagnation', '$SewageTreatment', '$WasteSegregation', '$DecentralizationSolidWaste', '$SolidWasteSuggestions','$neighborhoodSafetyNight', '$emergencyServicesSatisfaction', '$policeServicesSatisfaction', 
+  '$fireServicesSatisfaction', '$medicalServicesSatisfaction', '$educationalCulturalOpportunities', 
+  '$parksSatisfaction', '$distanceToPark', '$recreationalFacilities', '$climateChangeConcern', 
+  '$utilizationAbandonedQuarry', '$revampingWaterBodies'
+  ,'$desiltingDrainChannels', '$revitalizationWetlands', '$ecoPark', '$others', '$tourismCircuit', 
+  '$localBusinessInitiatives', '$additionalSuggestions')";
 
   if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
@@ -74,4 +78,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Error: " . $conn->error;
   }
 }
+
 $conn->close();
