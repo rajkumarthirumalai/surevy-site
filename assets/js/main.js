57d8bc36a7e9ff8ac247b5d88fb801b79e4b9077
rajkumar <rajkumarrajkumar061@gmail.com>
2024-02-18 17:46:16 +0530

    header changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -257,36 +257,36 @@
 
 })()
 $(function() {
-
-	if ( $('.owl-2').length > 0 ) {
-        $('.owl-2').owlCarousel({
-            center: false,
-            items: 1,
-            loop: true,
-            stagePadding: 0,
-            margin: 20,
-            smartSpeed: 8000,
-            autoplay: true,
-            nav: true,
-            dots: true,
-            pauseOnHover: true,
-            responsive:{
-                600:{
-                    margin: 20,
-                    nav: true,
+  if ($('.owl-2').length > 0) {
+      $('.owl-2').owlCarousel({
+          center: false,
+          items: 1,
+          loop: true,
+          stagePadding: 0,
+          margin: 20,
+          smartSpeed: 1000,
+          autoplay: true,
+          autoplayHoverPause: true, // Add this line to enable pause on hover
+          nav: true,
+          dots: true,
+          pauseOnHover: true,
+          responsive: {
+              600: {
+                  margin: 20,
+                  nav: true,
                   items: 2
-                },
-                1000:{
-                    margin: 20,
-                    stagePadding: 0,
-                    nav: true,
+              },
+              1000: {
+                  margin: 20,
+                  stagePadding: 0,
+                  nav: true,
                   items: 3
-                }
-            }
-        });            
-    }
+              }
+          }
+      });
+  }
+});
 
-})
 
 
 
